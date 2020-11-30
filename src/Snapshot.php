95e999561b7b6206867e8a4b223093327ebe05f4
sebrave <sebastian.ravenscroft@gmail.com>
2020-11-30 23:38:38 +0000

    Add option to generate line chart

--- src/Snapshot.php
+++ src/Snapshot.php
@@ -40,4 +40,48 @@ class Snapshot
             dump(env('APP_URL') . '/' . $filename . '.html');
         }
     }
+
+    public function draw(string $modelName, $columnX, $columnY)
+    {
+        if (class_exists($modelName)) {
+            $data = (new $modelName())::query()
+                ->orderBy($columnX)
+                ->get()
+                ->map(function ($model) use ($columnX, $columnY) {
+                    return [
+                        "x" => $model[$columnX],
+                        "y" => $model[$columnY]
+                    ];
+                });
+            dump($data);
+        } else {
+            $data = DB::table($modelName)
+                ->orderBy($columnX)
+                ->get()
+                ->map(function ($model) use ($columnX, $columnY) {
+                    return [
+                        "x" => $model->$columnX,
+                        "y" => $model->$columnY
+                    ];
+                });
+        }
+
+        $parts = explode("\\", $modelName);
+
+        $filename = 'snapshot_chart_' . strtolower($parts[count($parts) - 1]);
+
+        File::put(config('snapshot.output_folder') . $filename . '.html',
+            view('snapshot::data/chart')
+                ->with([
+                    'timestamp' => Carbon::parse(now()),
+                    'name' => $modelName,
+                    'data' => json_encode($data)
+                ])
+                ->render()
+        );
+
+        if (config('snapshot.dump_snapshot_link')) {
+            dump(env('APP_URL') . '/' . $filename . '.html');
+        }
+    }
 }
---
+++ src/resources/views/data/chart.blade.php
@@ -0,0 +1,44 @@
+<head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"
+            integrity="sha512-d9xgZrVZpmmQlfonhQUvTR7lMPtO7NkZMkA0ABN3PHCbKA5nqylQ/yWlFAyY6hYgdF1Qh6nYiuADWwKB4C2WSw=="
+            crossorigin="anonymous"></script>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <title>Chart for {{ $name }} table</title>
+</head>
+
+<div style="display: flex; align-content: center; justify-content: space-between;" class="p-5">
+    <h1 class="text-3xl">Chart showing {{ $name }} table</h1>
+    <p class="text-sm">{{ $timestamp }}</p>
+</div>
+
+<div class="px-3 py-4" style="width: 60%; min-width: 600px;">
+    <canvas id="myChart" height="400"></canvas>
+</div>
+
+
+<script>
+    var ctx = document.getElementById('myChart');
+
+    var scatterChart = new Chart(ctx, {
+        type: 'scatter',
+        data: {
+            datasets: [{
+                label: '{!! $name !!}',
+                data: {!! $data !!},
+                backgroundColor: 'rgba(100,100,255)'
+            }]
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    type: 'linear',
+                    position: 'bottom'
+                }]
+            },
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    });
+
+    scatterChart.update();
+</script>
